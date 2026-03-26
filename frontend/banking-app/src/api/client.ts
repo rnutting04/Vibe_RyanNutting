23e68fd3965bc521448e380c16ac30d9ac2d097a
rnutting04 <rnutting04@gmail.com>
2026-03-26 14:55:05 -0400

    added gunicorn

--- frontend/banking-app/src/api/client.ts
+++ frontend/banking-app/src/api/client.ts
@@ -1,5 +1,9 @@
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+
 export async function apiRequest(path: string, options: RequestInit = {}) {
-  const res = await fetch(`http://127.0.0.1:5000${path}`, {
+  const res = await fetch(`${API_URL}${path}`, {
     ...options,
     headers: {
       'Content-Type': 'application/json',
